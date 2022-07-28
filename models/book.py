a2498ed4fb13c37b6d190a4b2dc9ec12d01f516b
yuliannaromanova <r.yulianna777@gmail.com>
2022-07-28 12:34:15 +0300

    b

--- models/book.py
+++
@@ -1,3 +0,0 @@
-from unittest import result
-from config.mysqlconnection import connectToMySQL
-from flask import flash
--- models/user.py
+++
@@ -1,4 +0,0 @@
-from config.mysqlconnection import connectToMySQL
-import re
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-from flask import flash
